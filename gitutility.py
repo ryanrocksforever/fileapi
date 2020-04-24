e7fa1960fc313d4ae25cc43c19d6ab307c62e023
ryanrocksforever <3dmodelingmayarocks@gmail.com>
2020-04-24 09:21:43 -0700

    fix

--- gitutility.py
+++ gitutility.py
@@ -1,32 +1,3 @@
-# secondrevision
-
-from flask_api import FlaskAPI
-import sys
-import os
-
-from flask_cors import CORS
-from flask import Flask, request, abort, jsonify, send_from_directory
-from pathlib import Path
-from git import Repo
-import subprocess as cmd
-
-# from OpenSSL import SSL
-# context = SSL.Context(SSL.SSLv23_METHOD)
-# context.use_privatekey_file('server.key')
-# context.use_certificate_file('rootCA.pem')
-
-app = FlaskAPI(__name__)
-CORS(app)
-global running
-global alreadydone
-running = False
-alreadydone = False
-
-
-
-base_path = Path(__file__).parent
-
-
 import io
 import os
 import sys
@@ -150,129 +121,39 @@ def pull(repo, remote_name='origin', branch='master'):
             else:
                 raise AssertionError('Unknown merge analysis result')
 
-
+if __name__ == '__main__':
     # Cleanup
+    if os.path.exists(LOCAL_REPO_1):
+        shutil.rmtree(LOCAL_REPO_1)
+
+    if os.path.exists(LOCAL_REPO_2):
+        shutil.rmtree(LOCAL_REPO_2)
+
+    if os.path.exists(REMOTE_REPO):
+        shutil.rmtree(REMOTE_REPO)
+
+    # Initialize new remote repo
+    remote_repo = pygit2.init_repository(REMOTE_REPO, True)
+
+    # Clone local repo
+    local_repo_1 = pygit2.clone_repository(REMOTE_REPO,
+                                           LOCAL_REPO_1)
 
 
+    # Repo pull fastforwardable
+    create_commits(local_repo_1, 1)
+    push(local_repo_1)
 
 
+    # Repo pull merge necessary
+    create_commits(local_repo_1, 1)
+    create_commits(local_repo_2, 1)
+    push(local_repo_1)
 
+    pull(local_repo_1)
 
+    # Export files
+    archive_head(local_repo_1)
 
-
-
-
-@app.route('/', methods=["GET"])
-def api_root():
-    return {
-        "mom found the poop sock": "uh oh she found the pee drawer too"
-    }
-
-
-@app.route('/files', methods=["GET", "POST"])
-def files():
-    if request.method == "POST":
-
-        print("posting")
-        jsondata = request.data
-
-        if "True" in jsondata["download"]:
-            projpath = "./project/" + jsondata["name"]
-            projdir = (base_path / projpath).resolve()
-            #projdir = projdir.replace('//', '/')
-            print(projdir)
-            print("getting")
-            #projdir = "./project/" + jsondata["name"]
-            print("doodoo")
-            name = jsondata["name"] + ".py"
-            return send_from_directory(projdir, name, as_attachment=True)
-        else:
-            projpath1 = "./project/" + jsondata["name"]
-            projdir1 = (base_path / projpath1).resolve()
-            print(jsondata)
-            os.mkdir(projdir1)
-            print("made dir")
-            projpath2 = "./project/" + jsondata["name"] + "/" + jsondata["name"] + ".py"
-            projdir2 = (base_path / projpath2).resolve()
-
-            f = open(projdir2, "w")
-            f.write(jsondata["content"])
-            f.close()
-            print("made filetorun")
-            projpath3 = "./project/" + jsondata["name"] + "/" + "desc.txt"
-            projdir3 = (base_path / projpath3).resolve()
-            f = open(projdir3, "w")
-            f.write(jsondata["desc"])
-            f.close()
-            print("made desc")
-
-
-
-            if os.path.exists(REMOTE_REPO):
-                shutil.rmtree(REMOTE_REPO)
-
-            # Initialize new remote repo
-            remote_repo = pygit2.init_repository(REMOTE_REPO, True)
-
-            # Clone local repo
-
-
-
-            # Repo pull fastforwardable
-
-            create_commits(remote_repo, 1)
-            push(remote_repo)
-
-
-            # Repo pull merge necessary
-            c
-
-            pull(remote_repo)
-
-            # Export files
-            archive_head(remote_repo)
-
-
-
-            return {'create': "complete"}
-
-    if request.method == "GET":
-        print("getting")
-        file_path = (base_path / "./project").resolve()
-        folders = os.listdir(file_path)
-        print(folders)
-
-        b = {}
-
-        for i in folders:
-            # num = i[0]
-            name = i
-            print(i)
-            projpath = "./project/" + i + "/desc.txt"
-            projdir = (base_path / projpath).resolve()
-            f = open(projdir, "r")
-            desciption = f.read()
-            f.close()
-
-
-            b.__setitem__(name, desciption)
-
-        a = {"projects": b}
-        return a
-
-
-
-
-
-
-if __name__ == "__main__":
-    context = ('server.crt', 'server.key')
-    app.run(threaded=False, debug=False)
-
-# https://github.com/jasbur/RaspiWiFi
-# that is link to wifi setup thing i use
-# export FLASK_APP=fl-app.py
-# sudo -E flask run --host=switch-hub.local --port=80 --cert=adhoc
-# sudo -E flask run --host=switch-hub.local --port=80 --cert=server.crt --key=server.key
-
-# export FLASK_APP=fl-app.py && sudo -E flask run --host=switch-hub.local --port=80 --cert=server.crt --key=server.key
+    # TODO:
+    #   - Merge conflict resolution.
